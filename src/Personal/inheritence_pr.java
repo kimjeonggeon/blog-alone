1d482de0f52ee9974188cf98ed72c8ec80f0d1c2
jeonggeon <kjkkmk42397@gmail.com>
2023-06-08 00:47:06 +0900

    test2

---
+++ src/Personal/inheritence_pr.java
@@ -0,0 +1,24 @@
+package Personal;
+
+	class Point{
+		int x ;
+		int y;
+		
+	}
+	
+	 class Circle extends Point{
+		int r;
+	}
+
+	public  class inheritence_pr {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Circle c = new Circle();
+		c.x =1; 
+		c.y =2;
+		c.r=3;
+		
+		
+	}
+	
+}
